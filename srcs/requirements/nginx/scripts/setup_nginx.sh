c49b89da0360e9a1828ea9b98c37c86fb7bebb01
theosykas <theosykas@icloud.com>
2026-05-21 00:53:57 +0200

    yml_nginxService

--- srcs/requirements/nginx/scripts/setup_nginx.sh
+++ srcs/requirements/nginx/scripts/setup_nginx.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [! -d "name_dir"]; then
-	echo@"create ssl certificate"
+if [ ! -d "etc/ssl/private" ]; then
+	echo "create ssl certificate"
 	openssl req -x509 -nodes -newkey rsa:4096 -days 3650 \
-	-keyout /etc/ssl/private/ngnix.key \
-	-out  /etc/ssl/certs/crt_ngnix.crt \
+	-keyout /etc/ssl/private/nginx.key \
+	-out  /etc/ssl/certs/nginx.crt \
 	-sha256 \
-	-subj "/CN= $DOMAIN" && cat  $DOMAIN.crt  $DOMAIN.key >  $DOMAIN.pem
+	-subj "/CN=$DOMAIN"
+fi
+
+nginx -g "daemon off;"  # lunch serv ngnix
