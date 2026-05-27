74d6649e47a127aa558770e88011e61c6e26770d
theosykas <theosykas@icloud.com>
2026-05-27 11:41:59 +0200

    scripts_sh[WP]

--- srcs/requirements/wordpress/scripts/wordpress.sh
+++ srcs/requirements/wordpress/scripts/wordpress.sh
@@ -3,10 +3,44 @@
 # cas ou une commande ne fonctionne pas
 set -e
 
-# if [ ! -f ]
+cd /var/www/html
 
-wp user create
+if [ ! -f "wp-config.php" ]; then
+	echo "Wordpress not found, init in progress"
+	wp core download --allow-root
 
-# pid 1
-echo "lunching php-fpm"
-exec php-fpm
+	# create config.php with database(mariaDB) and check data_base content
+	wp config create \
+		--dbname="${MYSQL_DATABASE}" \
+		--dbuser="${MYSQL_USER}" \
+		--dbpass="${MYSQL_PASSWORD}" \
+		--dbhost="mariadb" \
+		--allow-root
+
+	# create admin user + init WP
+	wp core install \
+		--url=${DOMAIN_NAME} \
+		--title="inception_wp" \
+		--admin_user="${WP_USER_ADMIN}" \
+		--admin_password="${WP_ADMIN_PASSWD}" \
+		--admin_email="${WP_ADMIN_MAIL}" \
+		--allow-root
+
+	# guest user
+	wp user create \
+		"${WP_USER_GUEST}" \
+		"${WP_GUEST_MAIL}" \
+		--user_pass="${WP_GUEST_PASSWD}" \
+		--role=author \
+		--allow-root
+
+	echo "wordpress init completed succesfully"
+
+else
+	echo "wordpress already exist and configurated"
+
+fi
+
+echo "lunching PHP_FPM..."
+# -F enforce PID 1
+exec php-fpm8.2 -F
